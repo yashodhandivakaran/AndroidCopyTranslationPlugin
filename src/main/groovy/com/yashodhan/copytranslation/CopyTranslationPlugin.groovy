64882fda09db86633ddffe377e55b632350ce7b6
Yashodhan <yashodhan.divakaran@webyog.com>
2015-08-05 02:23:19 +0530

    Initial commit
    Signed-off-by: Yashodhan <yashodhan.divakaran@webyog.com>

---
+++ src/main/groovy/com/yashodhan/copytranslation/CopyTranslationPlugin.groovy
@@ -0,0 +1,35 @@
+package com.yashodhan.copytranslation
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Created by Yashodhan Divakaran on 01/08/15.
+ */
+class CopyTranslationPlugin implements Plugin<Project> {
+
+
+
+    @Override
+    void apply(Project project) {
+
+        //Add extension to project so that we can get some information from build
+        project.extensions.create(CopyTranslationPluginExtension.EXTENSION_NAME,CopyTranslationPluginExtension)
+
+        //This adds the task to project to which the plugin is applied
+        project.task(CopyTranslationTask.TASK_NAME,type:CopyTranslationTask){
+            project.afterEvaluate {
+                def extension = project.extensions.findByName(CopyTranslationPluginExtension.EXTENSION_NAME) as CopyTranslationPluginExtension
+
+                destPath = project.projectDir.absoluteFile.toString() + "/res"
+                destFileName = "strings.xml"
+
+                println("basepath " + extension.basePath)
+
+                sourcePath = extension.basePath
+                sourceFileName = extension.sourceFileName
+            }
+        }
+
+    }
+}
---
+++ src/main/groovy/com/yashodhan/copytranslation/CopyTranslationTask.groovy
@@ -0,0 +1,100 @@
+package com.yashodhan.copytranslation
+
+import org.apache.xerces.parsers.XMLParser
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+import java.nio.file.Path
+import java.nio.file.Paths
+
+/**
+ * Created by Yashodhan Divakaran on 31/07/15.
+ */
+class CopyTranslationTask extends DefaultTask {
+
+    public static final String TASK_NAME = "copyTranslation"
+
+    String sourcePath = null
+    String sourceFileName = null
+
+    String destPath = ""
+    String destFileName = "strings.xml"
+
+    String[] values = [
+          "values"
+        , "values-de"
+        , "values-es"
+        , "values-fr"
+        , "values-nl"
+        , "values-pt"
+        , "values-ru"
+        , "values-th"
+        , "values-zh-rCN"
+        , "values-zh-rTW"
+    ]
+
+
+    String string = "values"
+    String string_de = "values-de"
+    String string_es = "values-es"
+    String string_fr = "values-fr"
+    String string_nl = "values-nl"
+    String string_pt = "values-pt"
+    String string_ru = "values-ru"
+    String string_th = "values-th"
+    String string_zh_rCN = "values-zh-rCN"
+    String string_zh_rTW = "values-zh-rTW"
+
+
+    @TaskAction
+    void performTask(){
+        println(sourcePath+" : "+destPath)
+
+        values.each {
+            value ->
+                def source = sourcePath + "/" + value + "/" + sourceFileName
+                def destination = destPath + "/" + value + "/" + destFileName
+
+                copy(source, destination)
+                println("Copied ${value}/${sourceFileName}")
+        }
+        println("Copiead all translated strings.")
+    }
+
+    void copy(String sorucePath, String destinationPath) {
+
+        Path xmlSrcFilePath = Paths
+                .get(sorucePath)
+                .toAbsolutePath()
+
+        Path xmlDestFilePath = Paths
+                .get(destinationPath)
+                .toAbsolutePath()
+
+        def sourceRes = new XmlParser().parse(xmlSrcFilePath.toFile())
+        def destinationRes = new XmlParser().parse(xmlDestFilePath.toFile())
+
+        sourceRes.children().each {
+            string -> deleteDublicateString(destinationRes,string)
+                copyStringToDestination(destinationRes,string)
+        }
+
+        new XmlNodePrinter(new PrintWriter(new FileWriter(destinationPath))).print(destinationRes)
+    }
+
+    void deleteDublicateString(Node destNode,Node string) {
+
+        def duplicateStringNode = destNode.depthFirst().find {
+            dupString -> dupString.@name == string.@name
+        }
+
+        if(duplicateStringNode != null){
+            destNode.remove(duplicateStringNode)
+        }
+
+    }
+
+    void copyStringToDestination(Node destNode,Node string){
+        destNode.append(string)
+    }
+}
